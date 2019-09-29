24033dcd7acea5b850317e6ee89b958468e4e0a0
animevn <haanhth21@gmail.com>
2019-09-29 14:40:33 +0700

    Initial Commit

---
+++ MyLocation/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocation
+//
+//  Created by haanh on 9/29/19.
+//  Copyright © 2019 haanh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyLocation/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocation
+//
+//  Created by haanh on 9/29/19.
+//  Copyright © 2019 haanh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
