635b4336d02c381a7a0532bff9fe604d01997491
animevn <haanhth21@gmail.com>
2019-10-01 10:48:29 +0700

    add gesture tap, if user tap anything outside of first section, first row then keyboard hide

--- MyLocation/LocationDetail.swift
+++ MyLocation/LocationDetail.swift
@@ -14,6 +14,7 @@ class LocationDetailViewController:UITableViewController{
     var placemark:CLPlacemark!
     var categoryName = "Apple Store"
     
+    
     private func updateLabels(){
         
         lbDescription.text = ""
@@ -24,10 +25,25 @@ class LocationDetailViewController:UITableViewController{
         
     }
     
+    @objc private func hideKeyboard(_ gestureRecognizer: UIGestureRecognizer){
+        let point = gestureRecognizer.location(in: tableView)
+        let indexPath = tableView.indexPathForRow(at: point)
+        if indexPath != nil && indexPath!.section == 0 && indexPath?.row == 0{
+            return
+        }
+        lbDescription.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLabels()
         lbCategory.text = categoryName
+        
+        let gestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(hideKeyboard))
+        gestureRecognizer.cancelsTouchesInView = false
+        tableView.addGestureRecognizer(gestureRecognizer)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -49,6 +65,20 @@ class LocationDetailViewController:UITableViewController{
         guard let source = segue.source as? CategoryPickerViewController else {return}
         categoryName = source.selectedCategoryName
         lbCategory.text = categoryName
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if indexPath.section == 0 || indexPath.section == 1{
+            return indexPath
+        }else{
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 && indexPath.row == 0{
+            lbDescription.becomeFirstResponder()
+        }
     }
     
 }
